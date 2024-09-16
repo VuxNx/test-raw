f2998a76d75d04bf10b10cbab6798c023f81d6c0
VuxNx <158649859+VuxNx@users.noreply.github.com>
2024-09-16 11:00:54 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
+/*Default test*/
 // #include "XArrayListDemo.h"
-#include "unittest.h"
+
+/*Test for primitive type*/
+// #include "unittest.h"
+
+/*Test for user-defined type*/
+// #include "typetest.h"
+
+
 int main(){
-    TEST_RAW();
+    //TODO: call proper test method from corresponding module
     return 0;
 }
